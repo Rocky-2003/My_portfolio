6ac8af3932f8962a0dca7721f50ae28cd60847ff
Rocky Mor <rockymor2003@gmail.com>
2024-06-11 10:29:44 +0530

    done responsive ness and lazyloading

--- src/script.js
+++ src/script.js
@@ -1,17 +1,20 @@
 "use strict";
+import lazyLoading from "./lazyLoadingImg.js";
 
 const menuOpen = document.querySelector(".menu-icon");
 const menuClose = document.querySelector(".close-menu");
 const menu = document.querySelector(".menu");
+
+lazyLoading();
 
 menuOpen.addEventListener("click", (e) => {
   menu.classList.add("menu-hide");
   menuOpen.style.display = "none";
 });
 
-menuClose.addEventListener('click', (e) => {
-  console.log('clicked');
-  menu.classList.remove('menu-hide');
-  
-  menuOpen.style.display = '';
-})
+menuClose.addEventListener("click", (e) => {
+  console.log("clicked");
+  menu.classList.remove("menu-hide");
+
+  menuOpen.style.display = "";
+});
